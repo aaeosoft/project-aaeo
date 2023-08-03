d87a421f2b472401c4e64129e3ac2bb1a9155063
osahindev <osahin@outlook.com>
2023-08-03 10:27:30 +0300

    Create authenticate middleware

---
+++ server/src/middlewares/authenticate.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { NotAuthorizedError } from "../exceptions/authenticate/NotAuthorizedError";
+import { tokenService } from "../services";
+import { InvalidTokenError } from "../exceptions/authenticate/InvalidTokenError";
+
+const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization;
+
+        if (token === undefined) {
+            throw new NotAuthorizedError();
+        }
+
+        const data = tokenService().decode(token);
+
+        if(data === undefined) {
+            throw new InvalidTokenError();
+        }
+
+        req.user = data;
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+
+
+};
+
+export default authenticate;

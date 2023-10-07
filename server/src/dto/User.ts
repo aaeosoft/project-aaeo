906b4ab7e178e154693941aba947323a1528b310
osahindev <osahin@outlook.com>
2023-10-07 17:56:45 +0300

    Refactor User DTO

--- server/src/dto/User.ts
+++ server/src/dto/User.ts
@@ -2,78 +2,85 @@ import { Document } from "mongoose";
 import IUserSchema from "../interfaces/models/user.schema.interface";
 
 export class User {
-    private id: number | undefined;
-    private firstName: string;
-    private lastName: string;
-    private email: string;
-    private password: string;
-    private isActive: boolean;
+  private id: string | undefined;
+  private firstName: string;
+  private lastName: string;
+  private email: string;
+  private password: string;
+  private isActive: boolean;
 
-    constructor(
-        firstName: string,
-        lastName: string,
-        email: string,
-        password: string,
-        isActive: boolean = false,
-        id: number | undefined = undefined
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.isActive = isActive;
-        this.id = id;
-    }
+  constructor(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    isActive: boolean = false,
+    id: string | undefined = undefined
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.isActive = isActive;
+    this.id = id;
+  }
 
-    public setFirstName(firstName: string): User {
-        this.firstName = firstName;
+  public setFirstName(firstName: string): User {
+    this.firstName = firstName;
 
-        return this;
-    }
+    return this;
+  }
 
-    public setLastName(lastName: string): User {
-        this.lastName = lastName;
+  public setLastName(lastName: string): User {
+    this.lastName = lastName;
 
-        return this;
-    }
+    return this;
+  }
 
-    public setEmail(email: string): User {
-        this.email = email;
+  public setEmail(email: string): User {
+    this.email = email;
 
-        return this;
-    }
+    return this;
+  }
 
-    public setPassword(password: string): User {
-        this.password = password;
+  public setPassword(password: string): User {
+    this.password = password;
 
-        return this;
-    }
+    return this;
+  }
 
-    public getId(): number | undefined {
-        return this.id;
-    }
+  public getId(): string | undefined {
+    return this.id;
+  }
 
-    public getFirstName(): string {
-        return this.firstName;
-    }
+  public getFirstName(): string {
+    return this.firstName;
+  }
 
-    public getLastName(): string {
-        return this.lastName;
-    }
+  public getLastName(): string {
+    return this.lastName;
+  }
 
-    public getEmail(): string {
-        return this.email;
-    }
+  public getEmail(): string {
+    return this.email;
+  }
 
-    public getPassword(): string {
-        return this.password;
-    }
+  public getPassword(): string {
+    return this.password;
+  }
 
-    public getIsActive(): boolean {
-        return this.isActive;
-    }
+  public getIsActive(): boolean {
+    return this.isActive;
+  }
 
-    public static fromModel(user: IUserSchema): User {
-        return new User(user.firstName, user.lastName, user.email, user.password, user.isActive, user.id);
-    }
+  public static fromModel(user: IUserSchema): User {
+    return new User(
+      user.firstName,
+      user.lastName,
+      user.email,
+      user.password,
+      user.isActive,
+      user.id
+    );
+  }
 }

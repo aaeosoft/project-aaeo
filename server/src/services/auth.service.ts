8dc29f37d34e6864bc0d8389d3cad0b4bc135c82
osahindev <osahin@outlook.com>
2023-10-07 17:57:06 +0300

    Password Reset [WIP]

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -7,75 +7,105 @@ import { User } from "../dto/User";
 import { ICryptService } from "../interfaces/services/crypt.interface";
 import { UserEmailAlreadExistError } from "../exceptions/user/UserEmailAlreadExistError";
 import { UserNotCreatedError } from "../exceptions/user/UserNotCreatedError";
+import { PasswordResetRepository } from "../repositories/password_reset.repository";
+import { IPasswordResetRepository } from "../interfaces/repositories/password_reset.interface";
 
 export class AuthService implements IAuthService {
-    userRepository: IUserRepository;
-    tokenService: ITokenService;
-    cryptService: ICryptService;
+  userRepository: IUserRepository;
+  tokenService: ITokenService;
+  cryptService: ICryptService;
+  passwordResetRepository: IPasswordResetRepository;
 
-    constructor(userRepository: IUserRepository, tokenService: ITokenService, cryptService: ICryptService) {
-        this.userRepository = userRepository;
-        this.tokenService = tokenService;
-        this.cryptService = cryptService;
+  constructor(
+    userRepository: IUserRepository,
+    tokenService: ITokenService,
+    cryptService: ICryptService,
+    passwordResetRepository: IPasswordResetRepository
+  ) {
+    this.userRepository = userRepository;
+    this.tokenService = tokenService;
+    this.cryptService = cryptService;
+    this.passwordResetRepository = passwordResetRepository;
+  }
+
+  public async login(
+    email: string,
+    password: string
+  ): Promise<{ [key: string]: string | number }> {
+    const user = await this.userRepository.getByEmail(email);
+
+    if (!user) {
+      throw new UserNotFoundError();
     }
 
-    public async login(email: string, password: string): Promise<{ [key: string]: string | number }> {
-        const user = await this.userRepository.getByEmail(email);
-
-        if (!user) {
-            throw new UserNotFoundError();
-        }
-
-        if (!this.cryptService.verify_hashed(password, user.getPassword())) {
-            throw new UserWrongInformationError();
-        }
-
-        const accessToken = this.tokenService.encode({
-            "firstName": user.getFirstName(),
-            "lastName": user.getLastName(),
-            "email": user.getEmail(),
-        });
-
-        // Need refactor
-        const refreshToken = this.tokenService.encode({
-            "email": user.getEmail()
-        });
-
-        return {
-            "access-token": accessToken,
-            "refresh-token": refreshToken,
-        };
+    if (!this.cryptService.verify_hashed(password, user.getPassword())) {
+      throw new UserWrongInformationError();
     }
 
-    public async register(user: User): Promise<User> {
-        const user_exist = await this.userRepository.getEmailIsExist(user.getEmail());
+    const accessToken = this.tokenService.encode({
+      firstName: user.getFirstName(),
+      lastName: user.getLastName(),
+      email: user.getEmail(),
+    });
 
-        if (user_exist) {
-            throw new UserEmailAlreadExistError();
-        }
+    // Need refactor
+    const refreshToken = this.tokenService.encode({
+      email: user.getEmail(),
+    });
 
-        const userData = user.setPassword(
-            this.cryptService.hash_string(
-                user.getPassword()
-            )
-        );
+    return {
+      "access-token": accessToken,
+      "refresh-token": refreshToken,
+    };
+  }
 
-        const result = await this.userRepository.create(userData);
+  public async register(user: User): Promise<User> {
+    const user_exist = await this.userRepository.getEmailIsExist(
+      user.getEmail()
+    );
 
-        if (!(result instanceof User)) {
-            throw new UserNotCreatedError();
-        }
-
-        return result;
+    if (user_exist) {
+      throw new UserEmailAlreadExistError();
     }
 
-    public async forgotPassword(email: string): Promise<Boolean> {
-        const user = await this.userRepository.getByEmail(email);
+    const userData = user.setPassword(
+      this.cryptService.hash_string(user.getPassword())
+    );
 
-        if (user == null) {
-            throw new UserNotFoundError();
-        }
+    const result = await this.userRepository.create(userData);
 
-        return true;
+    if (!(result instanceof User)) {
+      throw new UserNotCreatedError();
     }
+
+    return result;
+  }
+
+  public async forgotPassword(email: string): Promise<Boolean> {
+    const user = await this.userRepository.getByEmail(email);
+
+    if (user == null) {
+      throw new UserNotFoundError();
+    }
+
+    const isExist = await this.passwordResetRepository.isExistToken(user);
+
+    if (isExist) {
+      await this.passwordResetRepository.deleteTokens(user);
+    }
+
+    const expire_time = 15 * 60;
+
+    const token = this.tokenService.encode(
+      {
+        email: user.getEmail(),
+      },
+      expire_time
+    );
+
+    const reset_token_data =
+      await this.passwordResetRepository.createResetPasswordToken(user, token);
+
+    return true;
+  }
 }
--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,24 +1,30 @@
 import { IAuthService } from "../interfaces/services/auth.interface";
 import { ICryptService } from "../interfaces/services/crypt.interface";
 import { ITokenService } from "../interfaces/services/token.interface";
+import { PasswordResetRepository } from "../repositories/password_reset.repository";
 import { UserRepository } from "../repositories/user.repository";
 import { AuthService } from "./auth.service";
 import { CryptService } from "./crypt.service";
 import { TokenService } from "./token.service";
 
-
 export const authService = (): IAuthService => {
-    const userRepository = new UserRepository();
-    const tService = tokenService();
-    const cService = cryptService();
+  const userRepository = new UserRepository();
+  const tService = tokenService();
+  const cService = cryptService();
+  const passwordRepository = new PasswordResetRepository();
 
-    return new AuthService(userRepository, tService, cService);
+  return new AuthService(
+    userRepository,
+    tService,
+    cService,
+    passwordRepository
+  );
 };
 
 export const cryptService = (): ICryptService => {
-    return new CryptService();
+  return new CryptService();
 };
 
 export const tokenService = (): ITokenService => {
-    return new TokenService();
+  return new TokenService();
 };
